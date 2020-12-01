b07dbc2ecb9cc70b8832623e2694ecddc08521f6
Andre LeBlanc <andrepleblanc@gmail.com>
2020-12-01 18:15:09 -0500

    day2 prep

--- day1.py
+++ day1.py
@@ -1,6 +1,7 @@
+from timeit import timeit
 import itertools
 import math
-import time
+
 
 nums = [int(x) for x in open('day1.txt').readlines()]
 
@@ -12,19 +13,15 @@ def sums2020(numbers, n):
 
 
 def part1():
-    return sums2020(nums, 2)
+    print(sums2020(nums, 2))
 
 
 def part2():
-    return sums2020(nums, 3)
+    print(sums2020(nums, 3))
 
 
 if __name__ == '__main__':
-    start = time.time()
-    print(part1())
-    end = time.time()
-    print("part1 in {}s". format(end-start))
-    start = time.time()
-    print(part2())
-    end = time.time()
-    print("part2 in {}s". format(end-start))
+    print("Part 1: ", end="")
+    print("- Time: ", timeit(part1, number=1))
+    print("Part 2: ", end="")
+    print("- Time: ", timeit(part2, number=1))
---
+++ day2.py
@@ -0,0 +1,20 @@
+from timeit import timeit
+
+
+# lines = open('day2.txt').readlines()
+numbers = [int(x) for x in open('day2.txt').readlines()]
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+if __name__ == '__main__':
+    print("Part 1: ", end="")
+    print("- Time: ", timeit(part1, number=1))
+    print("Part 2: ", end="")
+    print("- Time: ", timeit(part2, number=1))
