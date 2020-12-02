9dc4ccbc5b1e269d7086772844131807578a5745
Andre LeBlanc <andrepleblanc@gmail.com>
2020-12-02 07:07:54 -0500

    day2

--- day2.py
+++ day2.py
@@ -1,16 +1,28 @@
+import re
 from timeit import timeit
 
 
 # lines = open('day2.txt').readlines()
-numbers = [int(x) for x in open('day2.txt').readlines()]
+lines = [re.match(r'(\d+)-(\d+)\s([a-z]): (\w+)', x).groups() for x in open('day2.txt').readlines()]
+print(lines)
 
 
 def part1():
-    pass
+    valid = 0
+    for minc, maxc, letter, pw in lines:
+        if int(minc) <= pw.count(letter) <= int(maxc):
+            valid += 1
+    print(valid)
 
 
 def part2():
-    pass
+    valid = 0
+    for p1, p2, letter, pw in lines:
+        a = pw[int(p1)-1] == letter
+        b = pw[int(p2)-1] == letter
+        if (a and not b) or (b and not a):
+            valid += 1
+    print(valid)
 
 
 if __name__ == '__main__':
