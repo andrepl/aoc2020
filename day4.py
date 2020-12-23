407e2d47db28a57258ee658e2316d50e4423787c
Andre LeBlanc <andrepleblanc@gmail.com>
2020-12-23 11:45:22 -0500

    cleanup day 4

--- day4.py
+++ day4.py
@@ -1,59 +1,32 @@
 import re
 from timeit import timeit
 
-lines = open('day4.txt').readlines()
-lines = [l.strip() for l in lines]
+cardstrs = [re.split('\s+', c) for c in open('day4.txt').read().split('\n\n')]
+cards = [dict(c.split(':') for c in cs) for cs in cardstrs]
 
-cards = []
-pplines = []
-for line in lines:
-    if line == '':
-        cards.append(pplines)
-        pplines = []
-    else:
-        pplines.append(line)
-cards.append(pplines)
 
-print(cards)
-
-def parse_card(pplines):
-    pp = dict()
-    for line in pplines:
-        tups = line.split(' ')
-        for tup in tups:
-            k, v = tup.split(':', 1)
-            pp[k] = v
-    return pp
-
-def valhgt(v):
+def validate_height(v):
     match = re.match(r'(\d+)(in|cm)', v)
     if match is None:
         return False
     qty, unit = match.groups()
     qty = int(qty)
     if unit == 'cm':
-        if qty < 150:
-            return False
-        if qty > 193:
-            return False
+        return 150 <= qty <= 193
     elif unit == 'in':
-        if qty < 59:
-            return False
-        if qty > 76:
-            return False
-    else:
-        return False
-    return True
+        return 59 <= qty <= 76
+    return False
 
-REQ = 'byr iyr eyr hgt hcl ecl pid'.split(' ')
-REQ2 = dict(byr=lambda v: 1920 <= int(v) <= 2002,
-            iyr=lambda v: 2010 <= int(v) <= 2020,
-            eyr=lambda v: 2020 <= int(v) <= 2030,
-            hgt=valhgt,
-            hcl=lambda v: re.match(r'#[0-9a-f]{6}', v) is not None,
-            ecl=lambda v: v in 'amb blu brn gry grn hzl oth'.split(' '),
-            pid=lambda v: len(v) == 9 and re.match(r'^\d+$', v)
-            )
+
+REQ = dict(
+    byr=lambda v: 1920 <= int(v) <= 2002,
+    iyr=lambda v: 2010 <= int(v) <= 2020,
+    eyr=lambda v: 2020 <= int(v) <= 2030,
+    hgt=validate_height,
+    hcl=lambda v: re.match(r'#[0-9a-f]{6}', v) is not None,
+    ecl=lambda v: v in 'amb blu brn gry grn hzl oth'.split(' '),
+    pid=lambda v: len(v) == 9 and re.match(r'^\d+$', v)
+)
 
 
 def isvalid(c):
@@ -62,35 +35,23 @@ def isvalid(c):
             return False
     return True
 
+
 def isvalid2(c):
     if not isvalid(c):
-        print('False')
         return False
 
-    for k, validator in REQ2.items():
+    for k, validator in REQ.items():
         if not validator(c[k]):
-            print('False')
             return False
-    print('True')
     return True
 
 
 def part1():
-    valid = 0
-    for card in cards:
-        card = parse_card(card)
-        if isvalid(card):
-            valid += 1
-    print(valid)
+    print(len([c for c in cards if isvalid(c)]))
 
 
 def part2():
-    valid = 0
-    for i, card in enumerate(cards):
-        card = parse_card(card)
-        if isvalid2(card):
-            valid += 1
-    print(valid)
+    print(len([c for c in cards if isvalid2(c)]))
 
 
 if __name__ == '__main__':
